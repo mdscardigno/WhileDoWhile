8b69366654031a87158cac972823fec94e06df1b
Milagro Scardigno <mdscardigno@gmail.com>
2021-11-01 11:57:49 -0400

    continue on a while

--- Program.cs
+++ Program.cs
@@ -2,11 +2,25 @@
 
 namespace WhileDoWhile
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C# While Do While - Countdown");
+            int countdown = 10;
+            while (countdown > 0)
+            {
+                if (countdown == 4)
+                {
+                    countdown--;//we have to substract 1 or otherwise we will be stock in an infinite loop as the value of 4 does not change
+                    continue;
+                }
+                System.Console.WriteLine("[ " + countdown + "]");
+                countdown--;
+            }
+            Console.WriteLine("🚀 BLAST OFF!!!! 🚀");
+            Console.ReadKey();
+        }
+
     }
-  }
 }
